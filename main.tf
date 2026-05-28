930bf9280998f3b3a9ead97edef896df853bf6c4
nikitapatil1203 <nikita121212patil@gmail.com>
2026-05-28 12:42:34 +0530

    updated module path

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
 module "vpc" {
-  source     = "/Users/nikitapatil/Desktop/DevOps_Projects/Terraform_New_20May2026/modules/vpc"
+  source     = "./modules/vpc"
   cidr_block = var.cidr_block
   subnet_cidr_range = var.subnet_cidr_range
 }
 
 module "ec2_instance" {
-  source        = "/Users/nikitapatil/Desktop/DevOps_Projects/Terraform_New_20May2026/modules/ec2_instance"
+  source        = "./modules/ec2_instance"
   vpc_id        = module.vpc.vpc_id
   instance_type = var.instance_type
   ami_value     = var.ami_value
